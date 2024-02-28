8c5d9e1ef70033de3f51b5016c14d8d9cd213ec8
tranleminhhoang2410 <hoangtlm2410@gmail.com>
2024-02-28 22:54:14 +0700

    init book model

--- src/scripts/models/bookModel.js
+++ src/scripts/models/bookModel.js
@@ -1,17 +1,19 @@
-import { addBookService, getBooksService } from "../../services/bookServices";
+import { addBookService, getBooksService, deleteBookService, updateBookService } from '../services/bookServices';
 
 export default class BookModel {
-    constructor() {
-        this.books = [];
-    }
+	constructor() {
+		this.books = [];
+	}
 
-    addBook = async (book) => {
-        addBookService(book)
-        
-    }
+	addBook = async (bookData) => await addBookService(bookData);
 
-    getBooks = async () => {
-        const response = await getBooksService();
-        return response;
-    };
+	getBooks = async () => {
+		const response = await getBooksService();
+		this.books = response;
+		return this.books;
+	};
+
+	updateBook = async (bookId, bookData) => await updateBookService(bookId, bookData);
+
+	deleteBook = async (bookId) => await deleteBookService(bookId);
 }
